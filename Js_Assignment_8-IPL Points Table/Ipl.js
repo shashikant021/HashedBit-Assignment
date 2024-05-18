9d58c74838229b2951751a5e77e5293e4e4d8642
shashikant021 <166147275+shashikant021@users.noreply.github.com>
2024-05-18 09:01:20 +0530

    api all done

---
+++ Js_Assignment_8-IPL Points Table/Ipl.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(' https://my-json-server.typicode.com/FreSauce/json-ipl/data')
+      .then(response => response.json())
+      .then(data => {
+    
+        data.sort((a, b) => a.NRR - b.NRR);
+
+        const tbody = document.querySelector('tbody');
+
+        data.forEach((team, index) => {
+          const row = document.createElement('tr');
+  
+          row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${team.Team}</td>
+            <td>${team.Matches}</td>
+            <td>${team.Won}</td>
+            <td>${team.Lost}</td>
+            <td>${team.Tied}</td>
+            <td>${team.NRR}</td>
+            <td>${team.Points}</td>
+          `;
+  
+          tbody.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  });
+  
